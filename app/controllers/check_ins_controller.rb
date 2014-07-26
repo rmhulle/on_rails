81f87d93e87184e211c2575883114ef9e60cb438
Prelang Builder <builder@prelang.com>
2014-07-26 05:04:39 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,4 +1,8 @@
 class CheckInsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_check_in, only: [:show, :edit, :update, :destroy]
 
   # GET /check_ins
